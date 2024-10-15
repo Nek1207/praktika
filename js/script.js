e600bc539c020659e411b0e03e351fd3d955a0a1
Nek1207 <Nek1207@yandex.ru>
2024-10-15 23:05:28 +0500

    10/15/2024 version

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+
+// const body = document.body,
+//   scrollWrap = document.getElementsByClassName("content")[0],
+//   height = scrollWrap.getBoundingClientRect().height - 1,
+//   speed = 0.0275;
+
+// var offset = 0;
+
+// body.style.height = Math.floor(height) + "px";
+
+// function smoothScroll() {
+//     offset += (window.pageYOffset - offset) * speed;
+
+//     var scroll = "translateY(-" + offset + "px) translateZ(0)";
+//     scrollWrap.style.transform = scroll;
+
+//     callScroll = requestAnimationFrame(smoothScroll);
+// }
+
+// smoothScroll();
